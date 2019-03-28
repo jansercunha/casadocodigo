272716e2f790ddec42c133048e11b7222430d39b
Janser <Janser@Janser-PC>
2019-03-28 12:56:31 -0400

    Criado class pedido e transformando retorno da listagem em lista de pedido.

--- casadocodigo-master/src/main/java/br/com/casadocodigo/loja/controllers/PedidosServicoController.java
+++ casadocodigo-master/src/main/java/br/com/casadocodigo/loja/controllers/PedidosServicoController.java
@@ -1,15 +1,19 @@
 package br.com.casadocodigo.loja.controllers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
-import br.com.casadocodigo.loja.models.Produto;
+import br.com.casadocodigo.loja.models.Pedido;
 
 @Controller
 @RequestMapping(value="/pedidos", method=RequestMethod.GET)
@@ -22,9 +26,12 @@ public class PedidosServicoController {
 	public ModelAndView listar() {
 		String uri = "https://book-payment.herokuapp.com/orders";
 		
-		String response = restTemplate.getForObject(uri, String.class);
-		List<Pedido> pedidos = response;
+		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+		messageConverters.add(new MappingJackson2HttpMessageConverter());
+		restTemplate.setMessageConverters(messageConverters);
 		
+		List<Pedido> pedidos = Arrays.asList(restTemplate.getForObject(uri, Pedido[].class));
+				
 		ModelAndView modelAndView = new ModelAndView("pedidos/lista");
 		modelAndView.addObject("pedidos", pedidos);
 		return modelAndView;
---
+++ casadocodigo-master/src/main/java/br/com/casadocodigo/loja/models/Pedido.java
@@ -0,0 +1,61 @@
+package br.com.casadocodigo.loja.models;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.List;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+public class Pedido implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	@DateTimeFormat
+	private Calendar data;
+	
+	private BigDecimal valor;
+	
+	@ElementCollection
+	private List<Produto> produtos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Calendar getData() {
+		return data;
+	}
+
+	public void setData(Calendar data) {
+		this.data = data;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+}
